42e98df87bd98bcf97d10f42c5d758b3b3c795e7
Dario L Garcia <dario.garcia@kfgodel_dev>
2014-07-04 00:05:26 -0300

    Added new integration tests

---
+++ bean2bean/src/test/java/net/sf/kfgodel/bean2bean/integration/functional/converter/conversions/NestedObjectConversionTest.java
@@ -0,0 +1,26 @@
+package net.sf.kfgodel.bean2bean.integration.functional.converter.conversions;
+
+import org.junit.Test;
+
+/**
+ * This class verifies that when converting nested instances bean2bean behaves as expected
+ * Created by kfgodel on 04/07/14.
+ */
+public class NestedObjectConversionTest {
+
+    @Test
+    public void selfReferencedObjectToDto(){
+
+    }
+
+    @Test
+    public void selfReferenceInCollectionToDto(){
+
+    }
+
+    @Test
+    public void circularReferenceToDto(){
+
+    }
+
+}
---
+++ bean2bean/src/test/java/net/sf/kfgodel/bean2bean/integration/functional/converter/conversions/SetConversionTest.java
@@ -0,0 +1,8 @@
+package net.sf.kfgodel.bean2bean.integration.functional.converter.conversions;
+
+/**
+ * This class verifies that bean2bean converts Sets as expected when used as converter
+ * Created by kfgodel on 04/07/14.
+ */
+public class SetConversionTest {
+}
