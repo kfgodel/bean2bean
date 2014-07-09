193968a14a69b34a4561b7b786ca79f4f72082a3
Dario L Garcia <dario.garcia@kfgodel_dev>
2014-07-09 10:33:46 -0300

    Test cases for nary conversion

---
+++ bean2bean/src/test/java/net/sf/kfgodel/bean2bean/integration/functional/mapper/expressions/NAryExpressionsIT.java
@@ -0,0 +1,33 @@
+package net.sf.kfgodel.bean2bean.integration.functional.mapper.expressions;
+
+import org.junit.Test;
+
+/**
+ * This class verifies that b2b provides means to express mappings for map/reduce
+ * Created by kfgodel on 06/07/14.
+ */
+public class NAryExpressionsIT {
+
+    /**
+     * Map properties from one object to different positions on the list
+     */
+    @Test
+    public void objectPropertiesToListValues(){
+
+    }
+
+    @Test
+    public void listValuesToObjectProperties(){
+
+    }
+
+    @Test
+    public void objectToMapWithPropertyNamesAsKeysAndPropertyValuesAsValues(){
+
+    }
+
+    @Test
+    public void mapToObjectWithPropertyNamesAsKeysAndPropertyValuesAsValues(){
+
+    }
+}
