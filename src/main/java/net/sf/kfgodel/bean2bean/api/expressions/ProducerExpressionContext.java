1e70e7c2d6a0dbbc08d1964dcc1b8cd9d5161804
Dario L Garcia <dario.garcia@10pines.com>
2019-02-10 22:44:13 -0300

    Start version 3.0.0 from scratch

--- src/main/java/net/sf/kfgodel/bean2bean/api/expressions/ProducerExpressionContext.java
+++
@@ -1,8 +0,0 @@
-package net.sf.kfgodel.bean2bean.api.expressions;
-
-/**
- * This type represents a context for a producer expression
- * Created by kfgodel on 25/08/14.
- */
-public interface ProducerExpressionContext {
-}
--- src/main/java/net/sf/kfgodel/bean2bean/api/expressions/TransformerExpressionContext.java
+++
@@ -1,8 +0,0 @@
-package net.sf.kfgodel.bean2bean.api.expressions;
-
-/**
- * This type represents the context of a transformer operation
- * Created by kfgodel on 26/08/14.
- */
-public interface TransformerExpressionContext {
-}
