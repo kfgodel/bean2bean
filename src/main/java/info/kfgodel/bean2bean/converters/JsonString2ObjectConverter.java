350579721defb36896bed1b4d4117c4ba3323ca0
Dario L Garcia <dario.garcia@10pines.com>
2019-03-19 20:35:06 -0300

    Add json converters

---
+++ src/main/java/info/kfgodel/bean2bean/converters/JsonString2ObjectConverter.java
@@ -0,0 +1,48 @@
+package info.kfgodel.bean2bean.converters;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import info.kfgodel.bean2bean.core.api.Bean2beanTask;
+import info.kfgodel.bean2bean.core.api.exceptions.ConversionException;
+import info.kfgodel.bean2bean.core.impl.descriptor.ObjectDescriptor;
+
+import java.lang.reflect.Type;
+import java.util.function.BiFunction;
+
+/**
+ * This class interprets a JSON string input into the expected target type
+ * Date: 19/03/19 - 19:49
+ */
+public class JsonString2ObjectConverter implements BiFunction<String, Bean2beanTask, Object> {
+
+  private ObjectMapper jsonMapper;
+
+  @Override
+  public Object apply(String source, Bean2beanTask task) {
+    Type expectedType = task.getTargetType();
+    try {
+      return jsonMapper.readValue(source, getJacksonTypeRefFor(expectedType));
+    } catch (Exception e) {
+      ObjectDescriptor objectDescriptor = ObjectDescriptor.create();
+      String sourceDescription = objectDescriptor.describeInstance(source);
+      String targetDescription = objectDescriptor.describeTarget(expectedType, task.getConversionVector().getTarget());
+      throw new ConversionException("Failed to parse JSON from " + sourceDescription + " into " + targetDescription + ": " + e.getMessage() , task, e);
+    }
+  }
+
+  private TypeReference getJacksonTypeRefFor(Type expectedType) {
+    //Jackson doesn't accept Type as input, but does have its own type ref
+    return new TypeReference<Object>() {
+      @Override
+      public Type getType() {
+        return expectedType;
+      }
+    };
+  }
+
+  public static JsonString2ObjectConverter create() {
+    JsonString2ObjectConverter converter = new JsonString2ObjectConverter();
+    converter.jsonMapper = new ObjectMapper();
+    return converter;
+  }
+}
---
+++ src/main/java/info/kfgodel/bean2bean/converters/Object2JsonStringConverter.java
@@ -0,0 +1,34 @@
+package info.kfgodel.bean2bean.converters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import info.kfgodel.bean2bean.core.api.Bean2beanTask;
+import info.kfgodel.bean2bean.core.api.exceptions.ConversionException;
+import info.kfgodel.bean2bean.core.impl.descriptor.ObjectDescriptor;
+
+import java.util.function.BiFunction;
+
+/**
+ * This class converts any object into its JSON string representation
+ * Date: 19/03/19 - 19:50
+ */
+public class Object2JsonStringConverter implements BiFunction<Object, Bean2beanTask, String> {
+  private ObjectMapper jsonMapper;
+
+  @Override
+  public String apply(Object source, Bean2beanTask task) {
+    try {
+      return jsonMapper.writeValueAsString(source);
+    } catch (Exception e) {
+      ObjectDescriptor objectDescriptor = ObjectDescriptor.create();
+      String sourceDescription = objectDescriptor.describeSource(source, task.getConversionVector().getSource());
+      throw new ConversionException("Failed to generate JSON for " + sourceDescription+ ": " + e.getMessage(), task, e);
+    }
+  }
+
+  public static Object2JsonStringConverter create() {
+    Object2JsonStringConverter converter = new Object2JsonStringConverter();
+    converter.jsonMapper = new ObjectMapper();
+    return converter;
+  }
+
+}
