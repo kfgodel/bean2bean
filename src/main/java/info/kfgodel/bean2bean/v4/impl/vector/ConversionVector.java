5f672b3d5a38cdeade8821020fff826ecbcd256f
Dario L Garcia <dario.garcia@10pines.com>
2019-09-24 17:51:58 -0300

    Implementar vector

---
+++ src/main/java/info/kfgodel/bean2bean/v4/impl/vector/ConversionVector.java
@@ -0,0 +1,10 @@
+package info.kfgodel.bean2bean.v4.impl.vector;
+
+/**
+ * This interface represents a vector indicating direction for a conversion. It has source and a target
+ * to indicate what's the input and the expected output
+ *
+ * Date: 24/9/19 - 17:07
+ */
+public interface ConversionVector {
+}
--- src/test/java/info/kfgodel/bean2bean/v4/B2bTestContext.java
+++ src/test/java/info/kfgodel/bean2bean/v4/B2bTestContext.java
@@ -5,6 +5,7 @@ import info.kfgodel.bean2bean.v4.impl.engine.B2bEngine;
 import info.kfgodel.bean2bean.v4.impl.engine.ConversionIntent;
 import info.kfgodel.bean2bean.v4.impl.finder.ConverterFunctionFinder;
 import info.kfgodel.bean2bean.v4.impl.process.ConversionProcess;
+import info.kfgodel.bean2bean.v4.impl.vector.ConversionVector;
 
 import java.util.List;
 import java.util.function.Function;
@@ -35,6 +36,9 @@ public interface B2bTestContext extends TestContext {
 
   List<ConverterFunctionFinder> strategies();
   void strategies(Supplier<List<ConverterFunctionFinder>> definition);
+
+  ConversionVector vector();
+  void vector(Supplier<ConversionVector> definition);
 
 
 }
